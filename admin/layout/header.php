e2f9bc7e25d37620908e4e657e369d3e2ee176dc
KandeDwi <kandekembar@gmail.com>
2024-12-23 18:27:18 +0800

    first commit

--- admin/layout/header.php
+++ admin/layout/header.php
@@ -149,7 +149,7 @@ global $judul;
             </div>
             <div class="nav-item dropdown">
               <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
-                <span class="avatar avatar-sm" style="background-image: url(../../image/lord.jpeg )"></span>
+                <span class="avatar avatar-sm" style="background-image: url('assets/img/foto_pegawai)"></span>
                 <div class="d-none d-xl-block ps-2">
                   <div><?=$_SESSION['nama'] ?></div>
                   <div class="mt-1 small text-muted"><?=$_SESSION['jabatan'] ?></div>
